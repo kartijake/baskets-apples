6e1cb533b089430c3719677216b6891369e74c4a
kartijake <jkkarthik.kumar777@gmail.com>
2023-09-03 20:24:40 +0100

    drag and drop logic fixed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ const Basket = ({ width, height }, props) => {
 };
 
 const Apple = ({ radius }, props) => {
-  console.log("init");
   const circleGeometry = new THREE.CircleGeometry(radius, 128);
   const circleMaterial = new THREE.MeshBasicMaterial({ ...props });
   return new THREE.Mesh(circleGeometry, circleMaterial);
@@ -25,17 +24,16 @@ export default function App() {
   const [instance, setInstance] = useState([]);
   const [BasketsApple, setBasketsApple] = useState([]);
   const BasketAppleRef = useRef(BasketsApple);
-  const [type, setType] = useState("basket");
-  const typeRef = useRef(type);
   const [baskets, setBaskets] = useState([]);
   const basketsRef = useRef(baskets);
   const [scene, setScene] = useState(new THREE.Scene());
   let TableSize = { width: 20, height: 20 };
-  let AppleRadius=0.5
-  const mousePosition = new THREE.Vector2();
-  const [raycaster, setRaycaster] = useState(new THREE.Raycaster());
-  const rayCasterRef = useRef(raycaster);
-
+  let AppleRadius = 0.5;
+  
+  // const [raycaster, setRaycaster] = useState(new THREE.Raycaster());
+  // const rayCasterRef = useRef(raycaster);
+  const [drag, setDrag] = useState("");
+  const draggedRef = useRef(drag);
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -84,16 +82,9 @@ export default function App() {
     };
   }, []);
 
-  useEffect(() => {
-    typeRef.current = type;
-    rayCasterRef.current = raycaster;
-  }, [type]);
-
-  // useEffect(() => {
-  //   rayCasterRef.current = raycaster;
-  // }, [type]);
 
   useEffect(() => {
+    // rayCasterRef.current = raycaster;
     basketsRef.current = baskets;
     BasketAppleRef.current = BasketsApple;
   }, [baskets, BasketsApple]);
@@ -103,32 +94,34 @@ export default function App() {
       refContainer.current.removeChild(renderer.domElement);
     };
   }, []);
-  useEffect(() => {
-    if (!openModal) {
-      window.addEventListener("mousedown", HandleClick);
-    }
-    return () => {
-      window.removeEventListener("mousedown", HandleClick);
-    };
-  }, []);
 
-  const HandleClick = (e) => {
-    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
-    mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
-    let raycaster = rayCasterRef.current;
+  // useEffect(() => {
+  //   if (!openModal) {
+  //     window.addEventListener("mousedown", HandleClick);
+  //   }
+  //   return () => {
+  //     window.removeEventListener("mousedown", HandleClick);
+  //   };
+  // }, []);
+
+  const HandleClick = (clientX, clientY, draggedData) => {
+    const mousePosition = new THREE.Vector2();
+    mousePosition.x = (clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = -(clientY / window.innerHeight) * 2 + 1;
+    let raycaster = new THREE.Raycaster();
 
     raycaster.setFromCamera(mousePosition, camera);
 
     const currentBaskets = basketsRef.current;
+    console.log(mousePosition.x,mousePosition.y)
     let basketsIntersections = raycaster.intersectObjects(currentBaskets);
-
-    if (typeRef.current === "apple" && basketsIntersections.length > 0) {
+    if (draggedData === "apple" && basketsIntersections.length > 0) {
       addApple(basketsIntersections[0].object);
     }
 
-    if (typeRef.current === "basket") {
+    if (draggedData === "basket") {
       let intersects = raycaster.intersectObject(Table);
-
+      console.log(intersects)
       if (basketsIntersections.length > 0) {
         return toast("Already a basket exists");
       }
@@ -142,6 +135,7 @@ export default function App() {
 
   const CreateBasket = (data) => {
     const intersect = instance;
+    console.log(instance)
     const highlightPos = new THREE.Vector3()
       .copy(intersect.point)
       .floor()
@@ -168,8 +162,9 @@ export default function App() {
         side: THREE.DoubleSide,
       }
     );
+    console.log(highlightPos)
     newBasket.rotateX(-Math.PI / 2);
-    newBasket.position.set(highlightPos.x, 0, highlightPos.z);
+    newBasket.position.set(highlightPos.x, 0.1, highlightPos.z);
     const newBasketBoundingBox = new THREE.Box3().setFromObject(newBasket);
     const collidesWithExistingBasket = baskets.some((existingBasket) =>
       newBasketBoundingBox.intersectsBox(
@@ -202,7 +197,7 @@ export default function App() {
   };
 
   const addApple = (instance) => {
-    console.log("clicked");
+    console.log("clicked",instance);
     let appleRadius = AppleRadius;
     const apple = Apple(
       { radius: appleRadius },
@@ -239,7 +234,7 @@ export default function App() {
 
       apple.position.set(
         clickedBasket.position.x + basketWidth / 2 - appleRadius - offsetX,
-        0.1,
+        0.2,
         clickedBasket.position.z + basketHeight / 2 - appleRadius - offsetZ
       );
 
@@ -291,7 +286,7 @@ export default function App() {
 
       basket.position.set(
         -(currentX + basketWidth / 2),
-        0,
+        0.1,
         currentZ - basketHeight / 2
       );
       currentX += basketWidth + colSpacing;
@@ -301,21 +296,18 @@ export default function App() {
   };
 
   const addApplesToBaskets = () => {
-    // Iterate through the sorted list of baskets
     BasketAppleRef.current.forEach((basketData) => {
       const basket = basketData;
       const apples = basketData.apples;
 
-      
-      const appleRadius = AppleRadius
-      const yOffset = 0.1; 
+      const appleRadius = AppleRadius;
+      const yOffset = 0.2;
 
       let offsetX = 0;
       let offsetZ = 0;
       console.log(basket.geometry.parameters.height);
       let BasketWidth = basket.geometry.parameters.width;
       let BasketHeight = basket.geometry.parameters.height;
-      // console.log(basket)
       for (let i = 0; i < apples.length; i++) {
         const apple = apples[i];
 
@@ -326,54 +318,112 @@ export default function App() {
           offsetX = 0;
           offsetZ = offsetZ + appleRadius * 2;
         }
-        
+
         apple.position.set(
           basket.position.x + BasketWidth / 2 - appleRadius - offsetX,
           yOffset,
           basket.position.z + BasketHeight / 2 - appleRadius - offsetZ
         );
         scene.add(apple);
-        offsetX=offsetX+ (appleRadius*2) 
-        // console.log(apple)
-        // // Calculate the position of the apple inside the basket
-        // const xOffset =
-        //   -basket.geometry.parameters.width / 2 + i * (appleRadius * 2); // Adjust spacing as needed
-
-        // // Set the position of the apple inside the basket
-        // apple.position.set(
-        //   basket.position.x + xOffset + 0.5,
-        //   yOffset,
-        //   basket.position.z + 2.0
-        // );
-
-        // Add the apple to the scene
+        offsetX = offsetX + appleRadius * 2;
       }
     });
   };
 
-  // Combine these functions to sort, arrange, and add apples to the baskets
   const sortArrangeAndAddApples = () => {
     sortBasketsByAppleCount();
     arrangeBasketsTopLeft();
     addApplesToBaskets();
   };
 
+  // useEffect(() => {
+  //   const target = document.getElementById("droptarget");
 
+  //   target.addEventListener("dragover", (event) => {
+  //     event.preventDefault();
+  //   });
 
+  //   target.addEventListener("drop", (event) => {
+  //     event.preventDefault();
+  //     console.log(event)
+  //       // HandleClick(event);
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    // Get a reference to your drop target element
+    const dropTarget = document.getElementById("droptarget");
+
+    // Add an event listener for the dragover event
+    const handleDragOver = (e) => {
+      e.preventDefault();
+      // Add your custom styling or visual feedback
+    };
+    dropTarget.addEventListener("dragover", handleDragOver);
+
+    // Add an event listener for the drop event
+    const handleDrop = (e) => {
+      e.preventDefault();
+      const draggedData = e.dataTransfer.getData("text/plain");
+      const clientX = e.clientX;
+      const clientY = e.clientY;
+      HandleClick(clientX, clientY, draggedData);
+      // Add your custom logic or feedback
+    };
+    dropTarget.addEventListener("drop", handleDrop);
+
+    // Clean up the event listeners when the component unmounts
+    return () => {
+      dropTarget.removeEventListener("dragover", handleDragOver);
+      dropTarget.removeEventListener("drop", handleDrop);
+    };
+  }, [])
   return (
     <div>
-      <div ref={refContainer}></div>
+      <div
+        id="droptarget"
+        // onDrop={(e) => {
+        //   e.preventDefault();
+        //   const draggedData = e.dataTransfer.getData("text/plain"); // Get the drag data
+        //   console.log("Dropped data:", draggedData);
+        //   console.log("Dropped data:", e.clientX);
+        //   HandleClick(e.clientX, e.clientY, draggedData);
+        // }}
+        // onDragOver={(e) => {
+        //   e.preventDefault();
+        // }}
+        ref={refContainer}
+      ></div>
       <ModalSkeleton
         SubmissionHandler={CreateBasket}
         openModal={openModal}
         setOpenModal={setOpenModal}
         setInstance={setInstance}
       />
-      <div className="fixed h-[80vh] top-0 left-0 bg-white shadow-md px-5"> 
+      <div className="fixed h-[80vh] top-10 left-5 rounded-md bg-white shadow-md px-5">
+      
+          <div>
+        <div
+          draggable="true"
+          onDragStart={(e) => {
+            e.dataTransfer.setData("text/plain", "basket"); // Set the drag data
+          }}
+          className="w-14 h-14 bg-red-600"
+        > 
+                 
+        </div>
+            <span className="text-gray-700 text-sm"> Drag Drop to add Basket</span>
+          </div>
+        <div
+          draggable="true"
+          onDragStart={(e) => {
+            e.dataTransfer.setData("text/plain", "apple"); // Set the drag data
+          }}
+          className="w-7 h-7 rounded-full bg-blue-600"
+        >
+        </div>
 
-      <button onClick={() => setType("apple")}>add apple</button>
-      <button onClick={() => setType("basket")}>add Basket</button>
-      <button onClick={() => sortArrangeAndAddApples()}>Sort Baskets</button>
+  <button type="button" onClick={() => sortArrangeAndAddApples()} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Sort Baskets</button>
       </div>
     </div>
   );
